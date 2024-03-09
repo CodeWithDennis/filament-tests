ad09af86876636286ff9f0841e4a69a7b0d01c95
Dennis Elsinga <personal.elsinga@gmail.com>
2024-03-09 19:23:52 +0100

    feat: Add interactive prompts for creating resource tests

--- src/Commands/FilamentResourceTestsCommand.php
+++ src/Commands/FilamentResourceTestsCommand.php
@@ -3,17 +3,67 @@
 namespace CodeWithDennis\FilamentResourceTests\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\multiselect;
 
 class FilamentResourceTestsCommand extends Command
 {
-    public $signature = 'filament-resource-tests';
+    protected $signature = 'filament:test {resource?}';
 
-    public $description = 'My command';
+    protected $description = 'My command';
 
-    public function handle(): int
+    public function handle()
     {
-        $this->comment('All done');
+        $resource = (string) str(
+            $this->argument('resource') ?? text(
+            label: 'What is the resource you would like to create tests for?',
+            placeholder: 'DepartmentResource',
+            required: true,
+        ),
+        )
+            ->studly()
+            ->trim('/')
+            ->trim('\\')
+            ->trim(' ')
+            ->replace('/', '\\');
 
-        return self::SUCCESS;
+        if (!str($resource)->endsWith('Resource')) {
+            $resource .= 'Resource';
+        }
+
+        $modelName = Str::of($resource)->remove('Resource');
+
+        $options = [
+            'Create',
+            'Edit',
+            'List',
+            'View',
+            'Sort',
+            'Search',
+            'Filter'
+        ];
+
+        $tests = multiselect(
+            label: 'What tests would you like to create?',
+            options: $options,
+            default: $options,
+            scroll: 10,
+            required: true
+        );
+
+
+        // Sorting
+        // Searching
+        // Existence
+        // Filters
+        // Resetting filters
+        // Removing Filters
+
+
+        // Add a check icon
+        $this->info("Created tests for {$resource}.");
     }
+   
 }
