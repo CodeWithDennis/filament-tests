5563a1660e255f3237225e2ffa2d44b78948fd58
Dennis Elsinga <personal.elsinga@gmail.com>
2024-03-10 17:55:04 +0100

    Cleanup

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog
-
-All notable changes to `filament-resource-tests` will be documented in this file.
--- src/FilamentResourceTestsServiceProvider.php
+++ src/FilamentResourceTestsServiceProvider.php
@@ -10,16 +10,9 @@ class FilamentResourceTestsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-resource-tests')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament-resource-tests_table')
             ->hasCommand(FilamentResourceTestsCommand::class);
     }
 }
