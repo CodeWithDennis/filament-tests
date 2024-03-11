79c63baa5fcc6cb12733f1415636c48ab6996fa2
Dennis Elsinga <personal.elsinga@gmail.com>
2024-03-11 16:46:31 +0100

    refactor: Simplify getting and using resource name in commands

--- src/Commands/FilamentResourceTestsCommand.php
+++ src/Commands/FilamentResourceTestsCommand.php
@@ -62,9 +62,7 @@ class FilamentResourceTestsCommand extends Command
 
     protected function getSourceFilePath(): string
     {
-        $name = Str::of($this->argument('name'))->remove('Resource');
-
-        return base_path("tests/Feature/{$name}Test.php");
+        return base_path("tests/Feature/{$this->getResourceName()}Test.php");
     }
 
     protected function makeDirectory($path): string
@@ -126,12 +124,12 @@ class FilamentResourceTestsCommand extends Command
         $contents = $this->getSourceFile();
 
         if ($this->files->exists($path)) {
-            $this->warn("Test for {$this->getStubVariables()['resource']} already exists.");
+            $this->warn("Test for {$this->getResourceName()} already exists.");
             return;
         }
 
         $this->files->put($path, $contents);
-        $this->info("Test for {$this->getStubVariables()['resource']} created successfully.");
+        $this->info("Test for {$this->getResourceName()} created successfully.");
     }
 
 }
