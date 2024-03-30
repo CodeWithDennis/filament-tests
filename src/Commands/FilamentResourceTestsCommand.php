ea839e757ff8aea5efc01e3e6f6e9ba2a5830328
M. D <dissto@users.noreply.github.com>
2024-03-30 15:18:54 +0100

    Add name argument and --all and --force options

--- src/Commands/FilamentResourceTestsCommand.php
+++ src/Commands/FilamentResourceTestsCommand.php
@@ -19,7 +19,7 @@ use function Laravel\Prompts\multiselect;
 
 class FilamentResourceTestsCommand extends Command
 {
-    protected $signature = 'make:filament-resource-test';
+    protected $signature = 'make:filament-resource-test {name? : The name of the resource} {--f|force : Force overwrite the existing test} {--a|all : Create tests for all Filament resources}';
 
     protected $description = 'Create tests for a Filament components';
 
@@ -32,26 +32,55 @@ class FilamentResourceTestsCommand extends Command
         $this->files = $files;
     }
 
+    protected function normalizeResourceName(string $name): string
+    {
+        $name = ucfirst($name);
+
+        if (! str_contains($name, 'Resource')) {
+            $name .= 'Resource';
+        }
+
+        return $name;
+    }
+
     public function handle(): int
     {
         // Get the available resources
         $availableResources = $this->getResources()
             ->map(fn ($resource): string => str($resource)->afterLast('Resources\\'));
 
-        // Ask the user to select the resource they want to create a test for
-        $selectedResources = multiselect(
-            label: 'What is the resource you would like to create this test for?',
-            options: $availableResources->flatten(),
-            required: true,
-        );
+        if (! $this->argument('name')) {
+            if (! $this->option('all')) {
+                // Ask the user to select the resource they want to create a test for
+                $selectedResources = multiselect(
+                    label: 'What is the resource you would like to create this test for?',
+                    options: $availableResources->flatten(),
+                    required: true,
+                );
 
-        // Check if the first selected item is numeric (on windows without WSL multiselect returns an array of numeric strings)
-        if (is_numeric($selectedResources[0] ?? null)) {
-            // Convert the indexed selection back to the original resource path => resource name
-            $selectedResources = collect($selectedResources)
-                ->mapWithKeys(fn ($index) => [
-                    $availableResources->keys()->get($index) => $availableResources->get($availableResources->keys()->get($index)),
-                ]);
+                // Check if the first selected item is numeric (on windows without WSL multiselect returns an array of numeric strings)
+                if (is_numeric($selectedResources[0] ?? null)) {
+                    // Convert the indexed selection back to the original resource path => resource name
+                    $selectedResources = collect($selectedResources)
+                        ->mapWithKeys(fn ($index) => [
+                            $availableResources->keys()->get($index) => $availableResources->get($availableResources->keys()->get($index)),
+                        ]);
+                }
+            } else {
+                // User wants to create tests for all resources
+                $selectedResources = $availableResources->all();
+            }
+        } else {
+            // User supplied a resource name
+            $suppliedResourceName = $this->normalizeResourceName($this->argument('name'));
+
+            if (! $availableResources->contains($suppliedResourceName)) {
+                $this->error("The resource {$suppliedResourceName} does not exist.");
+
+                return self::FAILURE;
+            }
+
+            $selectedResources = [$availableResources->search($suppliedResourceName) => $suppliedResourceName];
         }
 
         foreach ($selectedResources as $selectedResource) {
@@ -68,9 +97,10 @@ class FilamentResourceTestsCommand extends Command
             // Get the contents of the test file
             $contents = $this->getSourceFile($resource);
 
-            // If the file already exists, ask the user if they want to overwrite it.
-            if ($this->files->exists($path)) {
-                if (! confirm("Test for {$selectedResource} already exists. Do you want to overwrite it?")) {
+            if ($this->files->exists($path) && ! $this->option('force')) {
+                // Ask the user if they want to overwrite the existing test
+                if (! confirm("The test for {$selectedResource} already exists. Do you want to overwrite it?")) {
+                    // Skip this resource
                     continue;
                 }
             }
