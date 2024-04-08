da0b08d1294e0418ec007a97400e664c9a395ee7
M. D. <dissto@users.noreply.github.com>
2024-04-09 00:51:09 +0200

    feature: Add group name resolver
    This PR adds a feature that automatically resolves the pest group method based on the stub path

--- src/Commands/FilamentTestsCommand.php
+++ src/Commands/FilamentTestsCommand.php
@@ -450,7 +450,7 @@ class FilamentTestsCommand extends Command
         $contents = '';
 
         foreach ($this->getStubs($resource) as $stub) {
-            $contents .= $this->getStubContents($stub, $this->getStubVariables($resource));
+            $contents .= $this->getStubContents($stub, $this->getStubVariables($resource, $stub));
         }
 
         return $contents;
@@ -544,13 +544,15 @@ class FilamentTestsCommand extends Command
         return $this->convertDoubleQuotedArrayString('['.implode(', ', $result).']');
     }
 
-    protected function getStubVariables(Resource $resource): array
+    protected function getStubVariables(Resource $resource, string $stubPath): array
     {
         $resourceModel = $resource->getModel();
         $userModel = User::class;
         $modelImport = $resourceModel === $userModel ? "use {$resourceModel};" : "use {$resourceModel};\nuse {$userModel};";
 
+
         $toBeConverted = [
+            'RESOLVED_GROUP_METHOD' => $this->getGroupMethod($stubPath),
             'RESOURCE_TABLE_COLUMNS' => $this->getTableColumns($resource)->keys(),
             'RESOURCE_TABLE_INITIALLY_VISIBLE_COLUMNS' => $this->getInitiallyVisibleColumns($resource)->keys(),
             'RESOURCE_TABLE_TOGGLED_HIDDEN_BY_DEFAULT_COLUMNS' => $this->getToggledHiddenByDefaultColumns($resource)->keys(),
@@ -588,5 +590,29 @@ class FilamentTestsCommand extends Command
     protected function getDeferredLoadingMethod(): string
     {
         return "\n\t\t->loadTable()";
+    }
+
+    protected function resolveGroupByStubPath(string $stubPath): ?string
+    {
+        $stubPath = str($stubPath)->replace('\\', '/');
+
+        $base = str($stubPath)->after('/stubs/')->beforeLast('/');
+
+        $base = str($base)->explode('/');
+
+        if (str($base->last())->contains('.stub')) {
+            $base->pop();
+        }
+
+        $groups = $base->map(fn($group) => str($group)->lower())->sort();
+
+        $groupsArray = $groups->toArray();
+
+        return  "'" . implode("','", $groupsArray) . "'";
+    }
+
+    protected function getGroupMethod(string $stubPath): string
+    {
+        return "->group({$this->resolveGroupByStubPath($stubPath)})";
     }
 }
