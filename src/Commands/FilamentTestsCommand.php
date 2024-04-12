d8a58c7c838299a7e007ecdf66aad6bae73bfd51
M. D. <dissto@users.noreply.github.com>
2024-04-12 21:34:59 +0200

    Remove color from the word Todos

--- src/Commands/FilamentTestsCommand.php
+++ src/Commands/FilamentTestsCommand.php
@@ -287,7 +287,7 @@ class FilamentTestsCommand extends Command
                     $this->components->twoColumnDetail('No. of Tests', $item['tests']);
 
                     if ($item['todos'] > 0) {
-                        $this->components->twoColumnDetail('  <fg=blue>Todos</>', '<fg=blue>'.$item['todos'].'</>');
+                        $this->components->twoColumnDetail('Todos', '<fg=blue>'.$item['todos'].'</>');
                     }
 
                     $this->components->twoColumnDetail('Duration', $item['duration']);
