50b79a126c3328df31fbad275c7df3b3597eec8a
dissto <dissto@users.noreply.github.com>
2024-06-13 12:47:38 +0000

    Pint: Fix code style issues

--- src/Stubs/Resource/Page/Create/Action/Render.php
+++ src/Stubs/Resource/Page/Create/Action/Render.php
@@ -8,7 +8,7 @@ use CodeWithDennis\FilamentTests\Stubs\Base;
 class Render extends Base
 {
     public Closure|bool $isTodo = true;
-    
+
     public function getDescription(): string
     {
         return 'can render action on the create page';
