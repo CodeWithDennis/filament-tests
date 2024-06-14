780c6b94895fefc326933b5d784aedb3ff7c00b9
dissto <dissto@users.noreply.github.com>
2024-06-14 09:47:54 +0000

    Pint: Fix code style issues

--- src/Commands/ListTodosCommand.php
+++ src/Commands/ListTodosCommand.php
@@ -1,78 +1,81 @@
 <?php
 
-    namespace CodeWithDennis\FilamentTests\Commands;
+namespace CodeWithDennis\FilamentTests\Commands;
 
-    use Illuminate\Console\Command;
-    use Illuminate\Filesystem\Filesystem;
-    use ReflectionClass;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use ReflectionClass;
 
-    class ListTodosCommand extends Command
+class ListTodosCommand extends Command
+{
+    protected $signature = 'make:filament-test-todo-list';
+
+    protected $description = '(internal) Output a list of todos for Filament tests';
+
+    protected Filesystem $files;
+
+    public function __construct(Filesystem $files)
     {
-        protected $signature = 'make:filament-test-todo-list';
-        protected $description = '(internal) Output a list of todos for Filament tests';
+        parent::__construct();
+        $this->files = $files;
+    }
 
-        protected Filesystem $files;
+    public function handle(): int
+    {
+        $files = $this->files->allFiles(__DIR__.'/../Stubs');
+        $todos = collect($files)
+            ->map(fn ($file) => $this->getClassFromFile($file->getPathname()))
+            ->filter(fn ($class) => $class && $this->hasTodoProperty($class))
+            ->values()
+            ->all();
 
-        public function __construct(Filesystem $files)
-        {
-            parent::__construct();
-            $this->files = $files;
-        }
+        $this->outputTodos($todos);
 
-        public function handle(): int
-        {
-            $files = $this->files->allFiles(__DIR__ . '/../Stubs');
-            $todos = collect($files)
-                ->map(fn($file) => $this->getClassFromFile($file->getPathname()))
-                ->filter(fn($class) => $class && $this->hasTodoProperty($class))
-                ->values()
-                ->all();
+        return self::SUCCESS;
+    }
 
-            $this->outputTodos($todos);
+    protected function getClassFromFile(string $filePath): ?string
+    {
+        $contents = $this->files->get($filePath);
+        $namespace = $this->getNamespaceFromFile($contents);
+        $className = $this->getClassNameFromFile($contents);
 
-            return self::SUCCESS;
-        }
+        return $namespace && $className ? $namespace.'\\'.$className : null;
+    }
 
-        protected function getClassFromFile(string $filePath): ?string
-        {
-            $contents = $this->files->get($filePath);
-            $namespace = $this->getNamespaceFromFile($contents);
-            $className = $this->getClassNameFromFile($contents);
+    protected function getNamespaceFromFile(string $fileContents): ?string
+    {
+        return str($fileContents)->match('/namespace\s+(.+?);/') ?: null;
+    }
 
-            return $namespace && $className ? $namespace . '\\' . $className : null;
-        }
+    protected function getClassNameFromFile(string $fileContents): ?string
+    {
+        return str($fileContents)->match('/class\s+(\w+)/') ?: null;
+    }
 
-        protected function getNamespaceFromFile(string $fileContents): ?string
-        {
-            return str($fileContents)->match('/namespace\s+(.+?);/') ?: null;
-        }
-
-        protected function getClassNameFromFile(string $fileContents): ?string
-        {
-            return str($fileContents)->match('/class\s+(\w+)/') ?: null;
-        }
-
-        protected function hasTodoProperty(string $class): bool
-        {
-            if (!class_exists($class)) {
-                return false;
-            }
-
-            $reflection = new ReflectionClass($class);
-            if ($reflection->hasProperty('isTodo')) {
-                $property = $reflection->getProperty('isTodo');
-                $instance = $reflection->newInstanceWithoutConstructor();
-                return $property->getValue($instance) === true;
-            }
-
+    protected function hasTodoProperty(string $class): bool
+    {
+        if (! class_exists($class)) {
             return false;
         }
 
-        protected function outputTodos(array $todos): void
-        {
-            $this->table(['Todos ' . count($todos)], collect($todos)->map(function ($todo) {
-                $todo = str($todo)->trim('CodeWithDennis\FilamentTests\Stubs\\');
-                return [$todo];
-            })->all());
+        $reflection = new ReflectionClass($class);
+        if ($reflection->hasProperty('isTodo')) {
+            $property = $reflection->getProperty('isTodo');
+            $instance = $reflection->newInstanceWithoutConstructor();
+
+            return $property->getValue($instance) === true;
         }
+
+        return false;
     }
+
+    protected function outputTodos(array $todos): void
+    {
+        $this->table(['Todos '.count($todos)], collect($todos)->map(function ($todo) {
+            $todo = str($todo)->trim('CodeWithDennis\FilamentTests\Stubs\\');
+
+            return [$todo];
+        })->all());
+    }
+}
