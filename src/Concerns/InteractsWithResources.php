f22029b35b004335c647632bb26f347ea452f4a1
CodeWithDennis <personal.elsinga@gmail.com>
2025-08-30 23:36:49 +0200

    Remove TODO comment from getResourceTableVisibleActions method

--- src/Concerns/InteractsWithResources.php
+++ src/Concerns/InteractsWithResources.php
@@ -59,7 +59,6 @@ trait InteractsWithResources
 
     public function getResourceTableVisibleActions(): array
     {
-        // TODO: Also add check for visible callback
         return array_filter($this->getResourceTableActions(), function (Action $action) {
             return ! $this->getPrivateProperty($action, 'isHidden');
         });
