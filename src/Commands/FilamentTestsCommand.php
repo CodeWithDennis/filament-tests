702c83f6ca1f65a263f437225e9d9f3f6610e34d
CodeWithDennis <personal.elsinga@gmail.com>
2025-08-30 21:16:41 +0200

    Add test renderers for create and edit pages

--- src/Commands/FilamentTestsCommand.php
+++ src/Commands/FilamentTestsCommand.php
@@ -2,6 +2,8 @@
 
 namespace CodeWithDennis\FilamentTests\Commands;
 
+use CodeWithDennis\FilamentTests\TestRenderers\CanRenderCreatePageTest;
+use CodeWithDennis\FilamentTests\TestRenderers\CanRenderEditPageTest;
 use Illuminate\Console\Command;
 
 class FilamentTestsCommand extends Command
@@ -10,8 +12,16 @@ class FilamentTestsCommand extends Command
 
     protected $description = 'Create a new test for a Filament component';
 
-    public function handle(): int
+    public function handle()
     {
-        // TODO
+        // TODO: THIS IS JUST TESTING STUFF
+        $resource = 'App\Filament\Admin\Resources\BadgeResource'; // Replace with dynamic input
+
+        $file = [
+            CanRenderCreatePageTest::build($resource)->render(),
+            CanRenderEditPageTest::build($resource)->render(),
+        ];
+
+        $this->info(implode("\n\n", $file));
     }
 }
