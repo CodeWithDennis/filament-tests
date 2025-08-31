cd3ab1b93e93f6ae179bcda7c1a2e80eaeb854cf
dissto <11778632+dissto@users.noreply.github.com>
2025-08-31 15:56:07 +0200

    Add isDiscoverable property to BeforeEach for discoverability control

--- src/TestRenderers/BeforeEach.php
+++ src/TestRenderers/BeforeEach.php
@@ -5,4 +5,6 @@ namespace CodeWithDennis\FilamentTests\TestRenderers;
 class BeforeEach extends BaseTest
 {
     public ?string $view = 'filament-tests::before-each';
+
+    public bool $isDiscoverable = false;
 }
