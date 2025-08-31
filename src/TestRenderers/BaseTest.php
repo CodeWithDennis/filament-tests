9e53b38ad2f126741b7c25dcc32293fb13337c0f
dissto <11778632+dissto@users.noreply.github.com>
2025-08-31 15:55:59 +0200

    Add CanBeDiscovered trait to BaseTest for enhanced discoverability management

--- src/TestRenderers/BaseTest.php
+++ src/TestRenderers/BaseTest.php
@@ -5,11 +5,13 @@ namespace CodeWithDennis\FilamentTests\TestRenderers;
 use CodeWithDennis\FilamentTests\Concerns\ExposesPublicMethodsToViews;
 use CodeWithDennis\FilamentTests\Concerns\HasFilamentResources;
 use CodeWithDennis\FilamentTests\Concerns\InteractsWithResources;
+use CodeWithDennis\FilamentTests\Concerns\Renderers\CanBeDiscovered;
 use CodeWithDennis\FilamentTests\Concerns\Renderers\CanBeSkipped;
 use CodeWithDennis\FilamentTests\Concerns\Renderers\CanBeTodo;
 
 abstract class BaseTest implements HasFilamentResources
 {
+    use CanBeDiscovered;
     use CanBeSkipped;
     use CanBeTodo;
     use CanRenderViews;
