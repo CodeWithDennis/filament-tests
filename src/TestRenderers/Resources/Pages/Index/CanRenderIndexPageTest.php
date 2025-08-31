01bd2a5e8ff9851ba73c4ca5ba78ce2d3ed61112
dissto <11778632+dissto@users.noreply.github.com>
2025-08-31 14:37:41 +0200

    Remove unnecessary getShouldRender method from CanRenderIndexPageTest

--- src/TestRenderers/Resources/Pages/Index/CanRenderIndexPageTest.php
+++ src/TestRenderers/Resources/Pages/Index/CanRenderIndexPageTest.php
@@ -7,9 +7,4 @@ use CodeWithDennis\FilamentTests\TestRenderers\BaseTest;
 class CanRenderIndexPageTest extends BaseTest
 {
     public ?string $view = 'filament-tests::resources.pages.index.can-render-index-page';
-
-    public function getShouldRender(): bool
-    {
-        return false;
-    }
 }
