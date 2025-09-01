d7d6cdb5f9e44819e1cdc6a0ff16df216082ef18
dissto <11778632+dissto@users.noreply.github.com>
2025-09-01 14:20:02 +0200

    Add HasGenerationSummary trait with showGenerationSummary method

---
+++ src/Concerns/Commands/HasGenerationSummary.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CodeWithDennis\FilamentTests\Concerns\Commands;
+
+trait HasGenerationSummary
+{
+    protected function showGenerationSummary(): void
+    {
+        //
+    }
+}
