7f81655034d9347d09a7d7b7e4863504d1501b5e
dissto <11778632+dissto@users.noreply.github.com>
2025-09-01 16:49:17 +0200

    Refactor RendersFilamentTests to return structured output with test count

--- src/Concerns/Commands/RendersFilamentTests.php
+++ src/Concerns/Commands/RendersFilamentTests.php
@@ -5,21 +5,24 @@ namespace CodeWithDennis\FilamentTests\Concerns\Commands;
 use CodeWithDennis\FilamentTests\TestRenderers\BaseTest;
 use CodeWithDennis\FilamentTests\TestRenderers\BeforeEach;
 use CodeWithDennis\FilamentTests\TestRenderers\Resources\Pages\Index\CanRenderIndexPageTest;
-use Illuminate\Support\Collection;
 
 trait RendersFilamentTests
 {
-    protected function renderTestsForResource(string $resource): string
+    protected function renderTestsForResource(string $resource): array
     {
-        /** @var Collection<class-string<BaseTest>> $renderers */
         $renderers = collect($this->getRenderers());
 
-        return $renderers
+        $output = $renderers
             ->map(fn (string $renderer) =>
                 /** @var BaseTest $renderer */
                 $renderer::build($resource)->render())
             ->prepend('<?php')
             ->implode("\n\n");
+
+        return [
+            'content' => $output,
+            'num_tests' => $renderers->count(),
+        ];
     }
 
     /**
