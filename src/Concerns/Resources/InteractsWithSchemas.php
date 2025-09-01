9ac0fb6ba4bdcd612771493c9715c246cd828d49
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-01 22:08:30 +0200

    Update form and infolist schemas with Livewire resources

--- src/Concerns/Resources/InteractsWithSchemas.php
+++ src/Concerns/Resources/InteractsWithSchemas.php
@@ -2,27 +2,33 @@
 
 namespace CodeWithDennis\FilamentTests\Concerns\Resources;
 
+use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\ViewRecord;
 use Filament\Schemas\Schema;
 
 trait InteractsWithSchemas
 {
     public function getResourceForm(): Schema
     {
-        return $this->getResource()->form(new Schema);
+        return $this->getResource()->form(new Schema(
+            app('livewire')->new(EditRecord::class)
+        ));
     }
 
     public function getResourceInfolist(): Schema
     {
-        return $this->getResource()->infolist(new Schema);
+        return $this->getResource()->infolist(new Schema(
+            app('livewire')->new(ViewRecord::class)
+        ));
     }
 
     public function getResourceInfolistFields(): array
     {
-        return $this->getResourceInfolist()->getFlatFields();
+        return $this->getResourceInfolist()->getFlatFields(true);
     }
 
     public function getResourceFormFields(): array
     {
-        return $this->getResourceForm()->getFlatFields();
+        return $this->getResourceForm()->getFlatFields(true);
     }
 }
