9c388f430066ba3a2339133df258d31c2c25db0a
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-01 21:28:13 +0200

    Refactor Livewire tests for edit and view page rendering

--- resources/views/resources/pages/edit/can-render-edit-page.blade.php
+++ resources/views/resources/pages/edit/can-render-edit-page.blade.php
@@ -2,5 +2,5 @@ it('can render the edit page', function (): void {
     $record = {{ $getResourceModel() }}::factory()->create();
 
     livewire({{ $getPageClass('edit') }}::class, ['record' => $record->id])
-    ->assertOk();
+        ->assertOk();
 });
--- resources/views/resources/pages/view/can-render-view-page.blade.php
+++ resources/views/resources/pages/view/can-render-view-page.blade.php
@@ -2,8 +2,5 @@ it('can render the view page', function (): void {
     $record = {{ $getResourceModel() }}::factory()->create();
 
     livewire({{ $getPageClass('view') }}::class, ['record' => $record->id])
-        ->assertOk()
-        ->assertSchemaStateSet([
-            //
-        ]);
+        ->assertOk();
 });
