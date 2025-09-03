dc677ef0594f0b7e6f0003cb85e6f503c638b325
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-03 19:57:51 +0200

    Add ability to defer loading of resource table

--- resources/views/resources/pages/index/can-sort-column.blade.php
+++ resources/views/resources/pages/index/can-sort-column.blade.php
@@ -2,6 +2,7 @@ it('can sort `:dataset` column', function (string $column): void {
     $records = {{ $getResourceModel() }}::factory(3)->create();
 
     livewire({{ $getPageClass('index') }}::class)
+        @if($isResourceTableLoadingDeferred())->loadTable()@endif
         ->sortTable($column)
         ->assertCanSeeTableRecords($records->sortBy($column), inOrder: true)
         ->sortTable($column, 'desc')
