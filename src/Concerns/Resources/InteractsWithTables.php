6e8af85adf2e68d4b6f938560ee96463e4ecd7b8
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-03 21:43:43 +0200

    Add CanSearchColumnTest for searching columns

--- src/Concerns/Resources/InteractsWithTables.php
+++ src/Concerns/Resources/InteractsWithTables.php
@@ -91,6 +91,17 @@ trait InteractsWithTables
         return $this->getResourceTableColumnKeysFrom($this->getResourceTableSortableColumns());
     }
 
+    public function getResourceTableSearchableColumns(): Collection
+    {
+        return $this->getResourceTableColumns()
+            ->filter(fn (Column $column): bool => $column->isSearchable());
+    }
+
+    public function getResourceTableSearchableColumnKeys(): array
+    {
+        return $this->getResourceTableColumnKeysFrom($this->getResourceTableSearchableColumns());
+    }
+
     public function getResourceTableActions(): array
     {
         return $this->getResourceTable()->getRecordActions();
