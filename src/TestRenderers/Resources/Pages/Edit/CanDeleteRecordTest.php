cd726ea2ca0d1b3a8a6ec93ac27f79ca5ad6b2f4
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-06 21:10:01 +0200

    Refactor test for deleting record visibility

--- src/TestRenderers/Resources/Pages/Edit/CanDeleteRecordTest.php
+++ src/TestRenderers/Resources/Pages/Edit/CanDeleteRecordTest.php
@@ -3,6 +3,7 @@
 namespace CodeWithDennis\FilamentTests\TestRenderers\Resources\Pages\Edit;
 
 use CodeWithDennis\FilamentTests\TestRenderers\BaseTest;
+use Filament\Actions\Action;
 
 class CanDeleteRecordTest extends BaseTest
 {
@@ -10,6 +11,9 @@ class CanDeleteRecordTest extends BaseTest
 
     public function getShouldRender(): bool
     {
-        return true; // TODO: Check if action exists.
+        return true;
+        /* TODO: We can't grab the delete action because it expects a record */
+        /* $action = $this->getPageHeaderAction(page: 'edit', action: 'delete'); */
+        /* return $action instanceof Action && $action->isVisible(); */
     }
 }
