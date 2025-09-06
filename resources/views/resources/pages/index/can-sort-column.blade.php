76a3b0e4a5eece2cef9bc84abed3c1a95a2dbc48
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-06 19:15:27 +0200

    Refactor sorting logic for `:dataset` column

--- resources/views/resources/pages/index/can-sort-column.blade.php
+++ resources/views/resources/pages/index/can-sort-column.blade.php
@@ -1,10 +1,14 @@
 it('can sort `:dataset` column', function (string $column): void {
     $records = {{ $getResourceModel() }}::factory(3)->create();
 
+    $sortingKey = data_get($records->first(), $column) instanceof BackedEnum
+        ? fn (Illuminate\Database\Eloquent\Model $record) => data_get($record, $column)->value
+        : $column;
+
     livewire({{ $getPageClass('index') }}::class)
         @if($isResourceTableLoadingDeferred())->loadTable()@endif
         ->sortTable($column)
-        ->assertCanSeeTableRecords($records->sortBy($column), inOrder: true)
+        ->assertCanSeeTableRecords($records->sortBy($sortingKey), inOrder: true)
         ->sortTable($column, 'desc')
-        ->assertCanSeeTableRecords($records->sortByDesc($column), inOrder: true);
+        ->assertCanSeeTableRecords($records->sortByDesc($sortingKey), inOrder: true);
 })->with([@foreach ($getResourceTableSortableColumnKeys() as $column)'{{ $column }}',@endforeach]);
