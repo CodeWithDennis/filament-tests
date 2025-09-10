3bdfdfa3cb44b2034c11ea82c028894e4b546bfc
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-10 15:43:49 +0200

    Rename form validation tests to match new naming convention

--- src/TestRenderers/Resources/Pages/Create/CanValidateCreateFormData.php
+++ src/TestRenderers/Resources/Pages/Create/CanValidateCreateFormData.php
@@ -4,9 +4,9 @@ namespace CodeWithDennis\FilamentTests\TestRenderers\Resources\Pages\Create;
 
 use CodeWithDennis\FilamentTests\TestRenderers\BaseTest;
 
-class CanValidateFormDataTest extends BaseTest
+class CanValidateCreateFormData extends BaseTest
 {
-    public ?string $view = 'filament-tests::resources.pages.create.can-validate-form-data';
+    public ?string $view = 'filament-tests::resources.pages.create.can-validate-create-form-data';
 
     public function getShouldRender(): bool
     {
