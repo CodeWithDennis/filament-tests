b97c299b7c5557251e21c4cc1cbc9bcf40ad2265
CodeWithDennis <personal.elsinga@gmail.com>
2025-09-10 15:18:59 +0200

    Add support for RichEditor in create record test

--- resources/views/resources/pages/create/can-create-record.blade.php
+++ resources/views/resources/pages/create/can-create-record.blade.php
@@ -1,10 +1,16 @@
+@php use Filament\Forms\Components\RichEditor; @endphp
 it('can create a record', function (): void {
     $record = {{ $getResourceModel() }}::factory()->make();
 
     livewire({{ $getPageClass('create') }}::class)
         ->fillForm([
             @foreach($getResourceFormFields() as $key => $field)
+                @if($field instanceof RichEditor)
+                    // TODO: RichEditor expects a very specific array structure for its data and Filament tests do not currently support this.
+                    // '{{ $key }}' => $record->{{ $key }},
+                @else
                 '{{ $key }}' => $record->{{ $key }},
+                @endif
             @endforeach
         ])
         ->call('create')
@@ -12,7 +18,12 @@ it('can create a record', function (): void {
 
         $this->assertDatabaseHas({{ $getResourceModel() }}::class, [
             @foreach($getResourceFormFields() as $key => $field)
+                @if($field instanceof RichEditor)
+                    // TODO: RichEditor expects a very specific array structure for its data and Filament tests do not currently support this.
+                    // '{{ $key }}' => $record->{{ $key }},
+                @else
                 '{{ $key }}' => $record->{{ $key }},
+                @endif
             @endforeach
         ]);
 });
